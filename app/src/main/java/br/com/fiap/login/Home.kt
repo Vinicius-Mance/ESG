4dd063e713d4ee5f5aa55b77fc835e0c97ae2ce0
Vinicius-Mance <vinimanceprof@gmail.com>
2022-03-09 23:51:14 -0300

    layout modificado

--- app/src/main/java/br/com/fiap/login/Home.kt
+++ app/src/main/java/br/com/fiap/login/Home.kt
@@ -1,11 +1,18 @@
 package br.com.fiap.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    fun openProfile(view : View){
+        val link = Intent(this, ProfileActivity::class.java)
+        startActivity(link)
     }
 }
